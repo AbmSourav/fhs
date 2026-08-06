7e76271d2b556042e5f0dcf93c1eb69d8da03681
AbmSourav <keramotul.islam@gmail.com>
2026-08-06 19:56:30 +0600

    logo live url added

--- fhs-app/resources/js/components/app-logo-icon.tsx
+++ fhs-app/resources/js/components/app-logo-icon.tsx
@@ -1,5 +1,5 @@
 import { ImgHTMLAttributes } from 'react';
 
 export default function AppLogoIcon(props: ImgHTMLAttributes<HTMLImageElement>) {
-    return <img src="/images/Fast-Home-Service.svg" alt="Fast Home Service" {...props} />;
+    return <img src="https://pub-a0a8f980dade4ee5a627f6949d4a890c.r2.dev/Fast-Home-Service.svg" alt="Fast Home Service" {...props} />;
 }
